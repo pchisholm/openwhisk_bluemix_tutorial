fdc533bb9bad9cf4bfd706c41cabecc29513b9e1
pchisholm <chisholm.p@gmail.com>
2017-05-02 15:54:35 -0300

    clean up es6 syntax in packages/javascript/generateTasks

--- packages/javascript/generateTasks/index.js
+++ packages/javascript/generateTasks/index.js
@@ -3,16 +3,16 @@
 
 function main(params) {
 	return new Promise((resolve, reject) => {
-	    let l = params.tasks.length;
+		let l = params.tasks.length, i = 4;
 
-	    for(var i = 4; i--;) {
-	        let idx = Math.floor(Math.random()*(l));
-	        let tmp = params.tasks[idx];
-	        params.tasks[idx] = params.tasks[i];
-	        params.tasks[i] = tmp;
-	    }
+		for (i; i--;) {
+			let idx = Math.floor(Math.random() * l);
+			let tmp = params.tasks[idx];
+			params.tasks[idx] = params.tasks[i];
+			params.tasks[i] = tmp;
+		}
 
-	    params.subset = params.tasks.slice(0, 4);
+		params.subset = params.tasks.slice(0, 4);
 
 		resolve(params);
 	});
